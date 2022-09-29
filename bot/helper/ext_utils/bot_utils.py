ed13d2f2aa007050339b73b4d46a6dd8f8d0848b
abhisekshh <114026963+abhisekshh@users.noreply.github.com>
2022-09-30 00:37:08 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -106,9 +106,9 @@ def get_progress_bar_string(status):
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
     cFull = p // 8
-    p_str = '⬢' * cFull
-    p_str += '⬡' * (12 - cFull)
-    p_str = f"【{p_str}】"
+    p_str = '🖕' * cFull
+    p_str += '❤️️' * (12 - cFull)
+    p_str = f"⦕{p_str}⦖"
     return p_str
 
 def get_readable_message():
